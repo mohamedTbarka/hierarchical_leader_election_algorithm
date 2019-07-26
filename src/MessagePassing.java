4718c06580c8dd3ab1aa45c43f2e0f4c6453ecfc
mohamed <simo.tbarka@gmail.com>
2019-07-26 22:48:12 +0200

    commit message !

--- src/MessagePassing.java
+++ src/MessagePassing.java
@@ -2,15 +2,22 @@ import io.jbotsim.core.Link;
 import io.jbotsim.core.Topology;
 import io.jbotsim.ui.JViewer;
 
+/**
+ * @author mohamed
+ * @project Hierarchical Leader Election Algorithm with Remoteness Constraint
+ */
+
 public class MessagePassing{
     public static void main(String[] args){
 
         Topology tp = new Topology();
-        tp.setDefaultNodeModel(LonerMessageBased.class);
+        tp.setDefaultNodeModel(LonerMessageBased_1.class);
+
         tp.setTimeUnit(500);
         tp.addNode(10, 10);
         tp.addNode(100, 100);
         new JViewer (tp);
         tp.start();
+        //tp.setl
     }
 }
